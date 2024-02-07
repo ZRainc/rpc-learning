f148bcc7ca80355eae5e4c983f7af5a7359eba51
zhangyuchen <823731949@qq.com>
2024-02-07 17:34:08 +0800

    init project

---
+++ rpc-grpc-demo/src/test/java/com/rain/rpc/rpcgrpcdemo/RpcGrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain.rpc.rpcgrpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcGrpcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
